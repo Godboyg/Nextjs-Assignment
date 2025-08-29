d629fd7d93cbbc4f1daf99b7425ebb83562eb98d
kushal <kushalsinghnegi72@gmail.com>
2025-08-29 22:37:54 +0530

    api changed

--- app/api/movies/[id]/route.ts
+++ app/api/movies/[id]/route.ts
@@ -2,21 +2,24 @@ import { connectDB } from "../../../../libs/db";
 import Movie, { Movietype } from "../../../../models/movie";
 import { NextResponse } from "next/server";
 
-export async function GET(_req: Request, { params }: { params: { id: string } }) {
+export async function GET(_req: Request, context: { params: { id: string } }) {
   await connectDB();
-  const movie: Movietype | null = await Movie.findById(params.id);
+  const { id } = context.params;
+  const movie: Movietype | null = await Movie.findById(id);
   return NextResponse.json(movie);
 }
 
-export async function PUT(request: Request, { params }: { params: { id: string } }) {
+export async function PUT(request: Request, context: { params: { id: string } }) {
   await connectDB();
   const data = await request.json();
-  const movie: Movietype | null = await Movie.findByIdAndUpdate(params.id, data, { new: true });
+  const { id } = context.params;
+  const movie: Movietype | null = await Movie.findByIdAndUpdate(id, data, { new: true });
   return NextResponse.json(movie);
 }
 
-export async function DELETE(_req: Request, { params }: { params: { id: string } }) {
+export async function DELETE(_req: Request, context: { params: { id: string } }) {
   await connectDB();
-  await Movie.findByIdAndDelete(params.id);
+  const { id } = context.params;
+  await Movie.findByIdAndDelete(id);
   return NextResponse.json({ message: "Movie deleted" });
 }
